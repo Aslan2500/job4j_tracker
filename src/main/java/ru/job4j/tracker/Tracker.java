e335b505f27b73a53ce8589b6b73a8ff0fd6983b
aslanmagamaev <aslanmagamaev@mail.ru>
2022-01-15 14:39:03 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -39,6 +39,19 @@ public class Tracker {
         }
     }
 
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return false;
+        } else {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+            items[items.length - 1] = null;
+            return true;
+        }
+    }
+
     public Item[] findAll() {
         return Arrays.copyOf(items, size);
     }
