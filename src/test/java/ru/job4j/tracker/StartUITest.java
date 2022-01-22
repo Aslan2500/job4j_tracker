c2b2a4debbb21f2aa31b502045fb0b20d9ab16d1
aslanmagamaev <aslanmagamaev@mail.ru>
2022-01-22 16:23:34 +0300

    9.2. Тесты вывода на консоль [#33585 #252681]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -87,7 +87,7 @@ public class StartUITest {
                         + "0. Show all Items" + ln
                         + "1. Exit" + ln
                         + "=== Show all items ===" + ln
-                        + "Item{id = 1, name = 'test1' created = " + created.format(FORMATTER) + "}" + ln
+                        + one + ln
                         + "Menu:" + ln
                         + "0. Show all Items" + ln
                         + "1. Exit" + ln
@@ -127,7 +127,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test1"));
         Input in = new StubInput(
-                new String[] {"0", "1", "1"}
+                new String[] {"0", String.valueOf(one.getId()), "1"}
         );
         UserAction[] actions = new UserAction[]{
                 new FindByIdAction(out),
@@ -140,7 +140,7 @@ public class StartUITest {
                         + "0. Find Item by it's id" + ln
                         + "1. Exit" + ln
                         + "=== Find item by id ===" + ln
-                        + "Item{id = 1, name = 'test1' created = " + created.format(FORMATTER) + "}" + ln
+                        + one + ln
                         + "Menu:" + ln
                         + "0. Find Item by it's id" + ln
                         + "1. Exit" + ln
