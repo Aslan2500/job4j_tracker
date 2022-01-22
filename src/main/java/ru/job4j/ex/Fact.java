050232c44ffdbea7c4fc466bfd85b0627e21261d
aslanmagamaev <aslanmagamaev@mail.ru>
2022-01-22 16:25:16 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #253185]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -8,7 +8,7 @@ public class Fact {
     public int calc(int n) {
         int rsl = 1;
         if (n < 0) {
-            throw new IllegalArgumentException("Number has to be higher than zero");
+            throw new IllegalArgumentException("Number has to be higher than zero or equal to zero");
         }
         for (int index = 1; index <= n; index++) {
             rsl *= index;
