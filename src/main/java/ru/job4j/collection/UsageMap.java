bccff9cd62710bf25055dd9d52a8b41081cf938f
aslanmagamaev <aslanmagamaev@mail.ru>
2022-01-29 12:05:33 +0300

    0. Map, HashMap [#212652 #257141]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,10 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> ourMap = new HashMap<>();
         ourMap.put("aslanmagamaev@mail.ru", "Aslan");
+        ourMap.put("aslanmagamaev@mail.ru", "Oleg");
+        ourMap.put("newmail@mail.ru", "Vlad");
+        ourMap.put("newmail@mail.ru", "Alex");
+        ourMap.put("anothermail@mail.ru", "Aslan");
         for (String key : ourMap.keySet()) {
             String value = ourMap.get(key);
             System.out.println(key + " = " + value);
